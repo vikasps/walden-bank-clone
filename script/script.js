c0ce25b60ab298a064e685694aacdbdd6405371f
vikasps <vikaspsmech18@gmail.com>
2021-10-04 23:50:30 +0530

    first-day- 1 page without responsive

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// <!-- Initialize Swiper -->
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// test swiper
+var swiper = new Swiper(".test_card.mySwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
